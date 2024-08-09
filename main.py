1fba9cde0f4c5314cd2cc95ddcd93e10989ae950
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-08-08 23:59:03 -0400

    feat: target website and scrape
    Points to a target website to scrape data (a website that invites scraping) and prints data.

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import mechanicalsoup
 import pandas as pd
 import sqlite3
 
-my_link = ""
+my_link = "https://www.scrapethissite.com/pages/simple/"
 
 browser = mechanicalsoup.StatefulBrowser()
 browser.open(my_link)
+
+#extract data
+my_country_div = browser.page.find_all("div", attrs={"class": "col-md-4 country"})
+
+# we use a list comprehension to isolate the text value of my country div
+distribution = [value.text for value in my_country_div]
+print(distribution)
