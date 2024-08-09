700d47b275f3cd6a0f7fcee124435832071a9219
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-08-09 13:37:42 -0400

    fix: complete the game loop
    Added game logic to keep user in game until they press "Q"

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# better_country_scraper.py
-
 # Sources: 
 # [1]: https://stackoverflow.com/questions/522563/how-to-access-the-index-value-in-a-for-loop
 # [2]: https://realpython.com/python-sleep/
@@ -9,6 +7,8 @@ import pandas as pd
 import sqlite3
 
 import time
+
+play_again = True
 
 my_link = "https://www.scrapethissite.com/pages/simple/"
 
@@ -31,10 +31,13 @@ def value_checker(distribution_to_check):
     print("\n\n\n")
 
 
-def play_game(data):
-  while(True):
-    print("Press a key to continue")
-    time.sleep(2)
+def play_game(data, has_chosen_to_play):
+  play_again = has_chosen_to_play
+  while(play_again):
+    player_choice = input("Press \'Q' to exit or another key to play the game\n")
+    if player_choice == "Q" or player_choice == "q":
+      play_again = False
+      print("Logging out the game - thanks for playing!\n")
 
 country_names = scrape_data("h3", "country-name", "")
 # country_infos = scrape_data("div", "country-info", "|||")
@@ -42,7 +45,7 @@ country_capitals = scrape_data("span", "country-capital", "|||")
 country_population = scrape_data("span", "country-population", "|||")
 country_area = scrape_data("span", "country-area", "|||")
 
-value_checker(country_capitals)
+# value_checker(country_capitals)
 # value_checker(country_population)
 # value_checker(country_area)
 
@@ -53,25 +56,10 @@ the_game_data_matrix[1] = country_capitals
 the_game_data_matrix[2] = country_population
 the_game_data_matrix[3] = country_area
 
+print("List of countries")
 
-play_game(the_game_data_matrix)
+for elem in country_names:
+  print(elem)
 
-# for i in range(len(distribution)):
-#   # curData = distribution[i][0]
-#   curData = distribution[i]
-#   # print("curData is ", curData)
-#   print("curData lenght is", str(len(curData)))
-#   the_game_data_matrix[i%4].append(curData)
-
-
-
-# my_print_target = the_game_data_matrix[0]
-# print(my_print_target)
-# print("\n\n\n\n SPACER \n\n\n\n")
-# my_print_target = the_game_data_matrix[1]
-# print(my_print_target)
-# print("\n\n")
-
-# for elem in my_print_target:
-  # print(elem)
-  # print("\n")
+play_game(the_game_data_matrix, play_again)
+ 
